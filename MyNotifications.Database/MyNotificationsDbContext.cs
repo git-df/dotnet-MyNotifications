3836a1285d9571db91e1cef2cf3d655d7b2e576c
dflorian <dawidflorian99@gmail.com>
2024-10-29 13:11:28 +0100

    Add Database project

---
+++ MyNotifications.Database/MyNotificationsDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyNotifications.DomainModel.Entities;
+
+namespace MyNotifications.Database;
+
+public class MyNotificationsDbContext : DbContext
+{
+    public DbSet<Log> Logs { get; set; }
+}
---
+++ MyNotifications.DomainModel/Entities/Log.cs
@@ -0,0 +1,9 @@
+﻿namespace MyNotifications.DomainModel.Entities;
+
+public class Log
+{
+    public int Id { get; set; }
+    public required string Source { get; set; }
+    public required string Value { get; set; }
+    public DateTime Created { get; set; }
+}
