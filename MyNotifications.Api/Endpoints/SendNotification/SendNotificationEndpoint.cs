7ec0a209f558be96bfc49f5e200aa821db5eaf46
Dawid Florian <dawidflorian99@gmail.com>
2024-11-06 22:41:05 +0100

    Implement Database

--- MyNotifications.Api/Endpoints/SendNotification/SendNotificationEndpoint.cs
+++ MyNotifications.Api/Endpoints/SendNotification/SendNotificationEndpoint.cs
@@ -1,11 +1,21 @@
 using FastEndpoints;
+using MyNotifications.Database;
+using MyNotifications.DomainModel.Entities;
 using MyNotifications.DomainModel.Events;
 using MyNotifications.DomainModel.Models;
+using Newtonsoft.Json;
 
 namespace MyNotifications.Api.Endpoints.SendNotification;
 
 public class SendNotificationEndpoint : Endpoint<SendNotificationRequest>
 {
+    private readonly MnDbContext _context;
+
+    public SendNotificationEndpoint(MnDbContext context)
+    {
+        _context = context;
+    }
+    
     public override void Configure()
     {
         Post("notifications");
@@ -14,10 +24,23 @@ public class SendNotificationEndpoint : Endpoint<SendNotificationRequest>
 
     public override async Task HandleAsync(SendNotificationRequest req, CancellationToken ct)
     {
-        var id = Guid.NewGuid();
+        var jsonRequest = JsonConvert.SerializeObject(req.Request, Formatting.Indented);
+        var notification = new Notification
+        {
+            Content = jsonRequest,
+            Created = DateTime.UtcNow,
+        };
+
+        _context.Notifications.Add(notification);
+        await _context.SaveChangesAsync(ct);
         
-        await PublishAsync(new DiscordNotificationEvent(id, req.Request), Mode.WaitForNone, ct);
-        await SendAsync(id, StatusCodes.Status201Created, ct);
+        if (notification.Id == Guid.Empty)
+        {
+            await SendErrorsAsync(cancellation: ct);
+        }
+        
+        await PublishAsync(new DiscordNotificationEvent(notification.Id, req.Request), Mode.WaitForNone, ct);
+        await SendAsync(notification.Id, StatusCodes.Status201Created, ct);
     }
 }
 
--- MyNotifications.Sender/Services/NotificationsService.cs
+++ MyNotifications.Sender/Services/NotificationsService.cs
@@ -1,19 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using MyNotifications.Database;
+using MyNotifications.DomainModel.Entities;
 using MyNotifications.DomainModel.Events;
 using MyNotifications.Sender.Interfaces;
+using Newtonsoft.Json;
 
 namespace MyNotifications.Sender.Services;
 
 public class NotificationsService : INotificationsService
 {
     private readonly IDiscordRepository _discordRepository;
+    private readonly MnDbContext _context;
 
-    public NotificationsService(IDiscordRepository discordRepository)
+    public NotificationsService(IDiscordRepository discordRepository, MnDbContext context)
     {
         _discordRepository = discordRepository;
+        _context = context;
     }
     
     public async Task ProcessNotificationEvent(DiscordNotificationEvent notificationEvent, CancellationToken ct = default)
     {
+        var notification = await _context.Notifications
+            .FirstOrDefaultAsync(x => x.Id == notificationEvent.Id, ct);
+        
+        var now = DateTime.UtcNow;
+        if (notification == null)
+        {
+            var jsonRequest = JsonConvert.SerializeObject(notificationEvent.Request, Formatting.Indented);
+            notification = new Notification
+            {
+                Content = jsonRequest,
+                Created = now,
+                Send = now
+            };
+            
+            _context.Notifications.Add(notification);
+        }
+        else
+        {
+            notification.Send = now;
+        }
+        
+        await _context.SaveChangesAsync(ct);
+        
         await _discordRepository.SendWebhook(notificationEvent.Request, ct);
     }
 }
